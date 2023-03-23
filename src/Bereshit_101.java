f89de3a0e9694beba5d1a89b4d9c4c50627ddf91
einav242 <e24022997b@gmail.com>
2023-03-23 14:12:20 +0200

    version 2

--- src/Bereshit_101.java
+++ src/Bereshit_101.java
@@ -17,7 +17,20 @@ public class Bereshit_101 {
 	public static final double MAIN_BURN = 0.15; //liter per sec, 12 liter per m'
 	public static final double SECOND_BURN = 0.009; //liter per sec 0.6 liter per m'
 	public static final double ALL_BURN = MAIN_BURN + 8*SECOND_BURN;
-	
+	double vs = 24.8;
+	double hs = 932;
+	double dist = 181*1000;
+	double ang = 58.3; // zero is vertical (as in landing)
+	double alt = 13748; // 2:25:40 (as in the simulation) // https://www.youtube.com/watch?v=JJ0VfRL9AMs
+	double time = 0;
+	double dt = 1; // sec
+	double acc=0; // Acceleration rate (m/s^2)
+	double fuel = 121; //
+	double weight = WEIGHT_EMP + fuel;
+	double NN = 0.7; // rate[0,1]
+	boolean start_landing = false;
+	boolean landed = false;
+	PID pid = null;
 	public static double accMax(double weight) {
 		return acc(weight, true,8);
 	}
@@ -29,66 +42,102 @@ public class Bereshit_101 {
 		return ans;
 	}
 	// 14095, 955.5, 24.8, 2.0
-	public static void main(String[] args) {
-		System.out.println("Simulating Bereshit's Landing:");
-		// starting point:
-		double vs = 24.8;
-		double hs = 932;
-		double dist = 181*1000;
-		double ang = 58.3; // zero is vertical (as in landing)
-		double alt = 13748; // 2:25:40 (as in the simulation) // https://www.youtube.com/watch?v=JJ0VfRL9AMs
-		double time = 0;
-		double dt = 1; // sec
-		double acc=0; // Acceleration rate (m/s^2)
-		double fuel = 121; // 
-		double weight = WEIGHT_EMP + fuel;
-		System.out.println("time, vs, hs, dist, alt, ang,weight,acc");
-		double NN = 0.7; // rate[0,1] 
-		// ***** main simulation loop ******
-		while(alt>0) {
-			if(time%10==0 || alt<100) {
-				System.out.println(time+","+vs+","+hs+","+dist+","+alt+","+ang+","+weight+","+acc);
-			}
-			// over 2 km above the ground
+
+	public void start(){
+		this.pid = new PID(0.1,0.01,0.5);
+		if(this.alt<2000){
+			this.start_landing = true;
+		}
+		if(!this.landed){
+			this.simulation();
+			this.computations();
+			this.print_data();
+		}
+	}
+
+	private void simulation(){
+		double update = this.pid.update(this.alt,this.dt, this.vs);
+		if(alt>0){
 			if(alt>2000) {	// maintain a vertical speed of [20-25] m/s
 				if(vs >25) {NN+=0.003*dt;} // more power for braking
 				if(vs <20) {NN-=0.003*dt;} // less power for braking
+				if(alt > 3500 && alt < 6000){
+					ang = 57.0;
+				}
+				else if (alt<3500){
+					ang = 54.0;
+				}
 			}
 			// lower than 2 km - horizontal speed should be close to zero
 			else {
-				if(ang>3) {ang-=3;} // rotate to vertical position.
-				else {ang =0;}
+				if(this.ang>3) {this.ang-=3.5;} // rotate to vertical position.
+				else {this.ang =0;}
 				NN=0.5; // brake slowly, a proper PID controller here is needed!
 				if(hs<2) {hs=0;}
+				if(alt>1000 && alt<1500){
+					NN = 0.4;
+				}
+				else if(alt>500)
+				{
+					NN = 0.5;
+				}
+				else if(alt>250)
+				{
+					NN = 0.55;
+				}
+				else if (alt > 125){
+					NN = 0.7;
+				}
 				if(alt<125) { // very close to the ground!
 					NN=1; // maximum braking!
-					if(vs<5) {NN=0.7;} // if it is slow enough - go easy on the brakes 
+					if(vs<5) {NN=0.2;} // if it is slow enough - go easy on the brakes
 				}
 			}
 			if(alt<5) { // no need to stop
-				NN=0.4;
+				NN=0;
 			}
-			// main computations
-			double ang_rad = Math.toRadians(ang);
-			double h_acc = Math.sin(ang_rad)*acc;
-			double v_acc = Math.cos(ang_rad)*acc;
-			double vacc = Moon.getAcc(hs);	
-			time+=dt;
-			double dw = dt*ALL_BURN*NN;
-			if(fuel>0) {
-				fuel -= dw;
-				weight = WEIGHT_EMP + fuel;
-				acc = NN* accMax(weight);
-			}
-			else { // ran out of fuel
-				acc=0;
-			}
-		
-			v_acc -= vacc; 
-			if(hs>0) {hs -= h_acc*dt;}
-			dist -= hs*dt;
-			vs -= v_acc*dt;
-			alt -= dt*vs; 
+		}
+		else {
+			this.landed = true;
+		}
+
+	}
+
+	private void computations(){
+		double ang_rad = Math.toRadians(ang);
+		double h_acc = Math.sin(ang_rad)*acc;
+		double v_acc = Math.cos(ang_rad)*acc;
+		double vacc = Moon.getAcc(hs);
+		time+=dt;
+		double dw = dt*ALL_BURN*NN;
+		if(fuel>0) {
+			fuel -= dw;
+			weight = WEIGHT_EMP + fuel;
+			acc = NN* accMax(weight);
+		}
+		else { // ran out of fuel
+			acc=0;
+		}
+
+		v_acc -= vacc;
+		if(hs>0) {hs -= h_acc*dt;}
+		dist -= hs*dt;
+		vs -= v_acc*dt;
+		alt -= dt*vs;
+	}
+
+	private void print_data(){
+		if(time%10==0 || alt<100) {
+			System.out.println("time: "+time+" vs: "+vs+" hs: "+hs+" alt: "+alt+" ang: "+this.ang+" weight: "+weight+" acc: "
+					+acc+" fuel: "+fuel+ " pid: "+ this.pid.update(this.alt,this.dt, this.vs)+" NN: "+ this.NN);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Simulating Bereshit's Landing:");
+		Bereshit_101 bereshit = new Bereshit_101();
+		while (!bereshit.landed){
+			bereshit.start();
 		}
 	}
 }
---
+++ src/PID.java
@@ -0,0 +1,54 @@
+
+public class PID {
+    private double P, I , D, last_error, p0, i0, d0;
+    private boolean first_run;
+    public PID(double p, double i, double d){
+        this.p0 = p;
+        this.i0 = i;
+        this.d0 = d;
+        this.I = 0;
+        this.first_run = true;
+    }
+    public double update(double error, double dt, double vs){
+        if (first_run){
+            last_error = error;
+            first_run = false;
+        }
+        this.P = error;
+        this.D = (error - this.last_error)/dt;
+        this.I+=error;
+        this.last_error = error;
+        double ans = this.P*this.p0 - this.I*this.i0 - this.D*this.d0;
+        return ans*0.001;
+    }
+
+public static void main(String[] args) {
+        double targetAltitude, currentAltitude, currentVelocity, currentAcceleration, dt;
+        double Kp = 0.1, Ki = 0.01, Kd = 0.5; // ערכי הקבועים לבקרת PID
+        double integral = 0, prevError = 0; // ערכי המשתנים לבקרת PID
+
+        System.out.print("Enter the target landing altitude: ");
+        targetAltitude = 0;
+
+        currentAltitude = 10000; // נניח שהחללית נמצאת בגובה 10,000 מטרים
+        currentVelocity = -500; // נניח שהחללית יורדת במהירות של 500 מטרים לשניה
+        currentAcceleration = 0;
+        dt = 0.1; // הזמן בין העדכונים לבקרת PID
+
+        while (currentAltitude > targetAltitude) {
+        double error = targetAltitude - currentAltitude; // חישוב השגיאה
+        integral += error * dt; // חישוב האינטגרל לבקרת PID
+        double derivative = (error - prevError) / dt; // חישוב הדרייבטיב לבקרת PID
+        double pidOutput = Kp * error + Ki * integral + Kd * derivative; // חישוב הפלט של הבקר PID
+
+        currentAcceleration = pidOutput; // עדכון האצת החללית לפי הפלט של הבקר PID
+        currentVelocity += currentAcceleration * dt; // עדכון המהירות של החללית לפי האצתה
+        currentAltitude += currentVelocity * dt; // עדכון הגובה של החללית לפי המהירות שלה
+        prevError = error; // שמירת השגיאה הקודמת לשימוש בחישוב הדרייבטיב בצעד הבא
+        }
+
+        System.out.println("Landing successful!");
+        System.out.println("Final altitude: " + currentAltitude);
+        System.out.println("Final velocity: " + currentVelocity);
+        }
+        }
